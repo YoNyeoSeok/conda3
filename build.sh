384fd6e7ccf719e8fd4b24a3dd69a817e31f294b
YoNyeoSeok <yys8646@postech.ac.kr>
2021-05-09 02:04:41 +0900

    UPDATE cuda 11.2.1, 11.2.2 with ubuntu18.04

--- build.sh
+++ build.sh
@@ -3,12 +3,22 @@ IMAGE=yonyeoseok/conda3
 NAME=conda3
 
 # for CUDA in 10.0 9.0
+# do
 # 	TAG=cuda$CUDA-cudnn7-devel-ubuntu16.04
 # 	docker build -t $IMAGE:$TAG --build-arg CUDA=$CUDA .
 # 	docker push $IMAGE:$TAG
-for CUDA in 11.2.1
+# done
+
+# for CUDA in 11.2.1
+# do
+# 	TAG=$CUDA-cudnn8-devel-ubuntu16.04
+# 	docker build -t $IMAGE:cuda$TAG --build-arg TAG=$TAG .
+# 	docker push $IMAGE:cuda$TAG
+# done
+
+for CUDA in 11.2.1 11.2.2
 do
-	TAG=$CUDA-cudnn8-devel-ubuntu16.04
+	TAG=$CUDA-cudnn8-devel-ubuntu18.04
 	docker build -t $IMAGE:cuda$TAG --build-arg TAG=$TAG .
 	docker push $IMAGE:cuda$TAG
 done
